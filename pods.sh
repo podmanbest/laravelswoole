3aeb3e47be79589b98bb6d1474c6dca3386aba07
Asep Ready <savalunova@gmail.com>
2025-06-09 07:50:27 -0400

    podman

--- pods.sh
+++ pods.sh
@@ -1,1 +1,12 @@
+# build images
+podman build -t laravel-app ./laravel-app
+
+# check if podman is installed
+podman pod stop laravel-pod 2>/dev/null || true
+
+if podman pod exists laravel-pod; then
+  podman pod rm -f laravel-pod
+fi
+podman pod create --name laravel-pod
+
 podman play kube ./pods.yaml
